d4b38d6171e551cea27ea943542e4dfed63205d4
Cristian Gauxachs <cristian@gauxachs.com>
2025-06-30 09:28:02 +0200

    fix(ci): Try to fix the CIs

--- angular.webpack.js
+++ angular.webpack.js
@@ -1,10 +1,12 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires,no-undef
-const webpack = require('webpack');
-
 /**
  * Custom angular webpack configuration
  */
+// eslint-disable-next-line @typescript-eslint/no-var-requires,no-undef
 module.exports = (config, options) => {
+  // Use webpack from Angular's bundled version
+  // eslint-disable-next-line @typescript-eslint/no-var-requires,no-undef
+  const webpack = require('@angular-devkit/build-angular/node_modules/webpack');
+  
   config.plugins = [
     ...config.plugins,
     new webpack.IgnorePlugin({
